d261dc7cedef6bf6987bdb6e473c83dec9182c49
sermalp <76241106+sermalp@users.noreply.github.com>
2020-12-23 18:12:12 +0300

    Добавление моделей в интерфейс админки

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Регистрация модулей в админке
+from.models import *
+
+admin.site.register(Category)
+admin.site.register(Notebook)
+admin.site.register(Smartphone)
+admin.site.register(CardProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
