4f0399e13b6fe40ce1b76af4be0d572ad06f7f32
sermalp <76241106+sermalp@users.noreply.github.com>
2020-12-23 21:58:41 +0300

    Ограничение на загрузку изображение: (01:40:11)
    Ограничение на максимальное разрешение и размер

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,28 +1,31 @@
+from PIL import Image
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from.models import *
-from PIL import Image
+
 
 class NotebookAdminForm(ModelForm):
 
-    MIN_RESOLUTION = (300, 300)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = 'Минимальное разрешение изображения {}х{}'.format(
-            *self.MIN_RESOLUTION
+        self.fields['image'].help_text = mark_safe('<span style="color:red; font-size:14px">Минимальное разрешение изображения {}х{}</span>').format(
+            *Product.MIN_RESOLUTION
         )
 
     def clean_image(self):
         image = self.cleaned_data['image']
         img = Image.open(image)
         # print(img.width, img.height, img)
-        min_height, min_width = self.MIN_RESOLUTION
-        if img.height < min_height or img.width < min_width:
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображения не больше 3Мб')
+        if img.height < max_height or img.width < min_width:
             raise ValidationError('Разрешение изображения меньше минимального')
+        if img.height < max_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения больше максимального')
         return image
-
-
 
 
 class NotebookAdmin(admin.ModelAdmin):
@@ -35,13 +38,13 @@ class NotebookAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field,request, **kwargs)
 
 
-
 class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field,request, **kwargs)
+
 
 # Регистрация модулей в админке
 admin.site.register(Category)
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
@@ -15,6 +16,14 @@ User = get_user_model()
 # *************
 # 6 Customer - Покупатель
 # 7 Specification - Характеристики товаров
+
+
+class MinResolutinErrorException(Exception):
+    pass
+
+
+class MaxResolutinErrorException(Exception):
+    pass
 
 
 class LatestProductsManager:
@@ -54,6 +63,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (300, 300)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -66,6 +79,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutinErrorException('Разрешение изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutinErrorException('Разрешение изображения больше максимального')
+        return image
 
 
 class Notebook(Product):
