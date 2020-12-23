9e5a0b15746e7368f7358aaa22f4a81b83391db9
sermalp <76241106+sermalp@users.noreply.github.com>
2020-12-23 23:44:41 +0300

    Model View Template: (02:06:56)
    свой шаблон html в зависимости от категории продукта

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,4 +1,4 @@
-from PIL import Image
+#from PIL import Image
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
 from django.utils.safestring import mark_safe
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from io import BytesIO
+from django.urls import reverse
 
 
 User = get_user_model()
@@ -19,6 +20,11 @@ User = get_user_model()
 # *************
 # 6 Customer - Покупатель
 # 7 Specification - Характеристики товаров
+
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug':obj.slug})
 
 
 class MinResolutinErrorException(Exception):
@@ -93,8 +99,6 @@ class Product(models.Model):
             raise MinResolutinErrorException('Разрешение изображения меньше минимального')
         if img.height > max_height or img.width > max_width:
             raise MaxResolutinErrorException('Разрешение изображения больше максимального')
-
-
         # # пример с обрезкой изображения до нужного разрешения
         # image = self.image
         # img = Image.open(image)
@@ -125,6 +129,9 @@ class Notebook(Product):
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 class Smartphone(Product):
 
@@ -141,6 +148,9 @@ class Smartphone(Product):
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 class CardProduct(models.Model):
 
@@ -156,6 +166,7 @@ class CardProduct(models.Model):
 
     def __str__(self):
         return "Продукт: {} (для корзины)".format(self.product.title)
+
 
 class Cart(models.Model):
 
@@ -176,6 +187,7 @@ class Customer(models.Model):
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.fitst_name, self.user.last_name)
+
 
 class Specification(models.Model):
 
