c5e6fbfa4a597b6638193354ffe65c93e0dabc40
sermalp <76241106+sermalp@users.noreply.github.com>
2020-12-23 18:39:53 +0300

    Чтобы в карточке Notebook можно было выбрать только категорию Ноутбуки - поиск по значения в поле slug.
    Выбор категории в карточке Продукта по фильтрованному списку: если для какой-то категории закодирована таблица характеристик.

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,13 +1,37 @@
+from django import forms
 from django.contrib import admin
-
-# Register your models here.
-
-# Регистрация модулей в админке
 from.models import *
 
+
+class NotebookCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field,request, **kwargs)
+
+
+class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field,request, **kwargs)
+
+# Регистрация модулей в админке
 admin.site.register(Category)
-admin.site.register(Notebook)
-admin.site.register(Smartphone)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
 admin.site.register(CardProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
+
+
