7287504bde847e3c1925c64665ed3bc096f0c0d0
sermalp <76241106+sermalp@users.noreply.github.com>
2020-12-23 21:26:37 +0300

    Работа с изображениями: (01:30:27)
    Загрузка изображения - минимальное разрешение

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,10 +1,33 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
-
 from.models import *
+from PIL import Image
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (300, 300)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Минимальное разрешение изображения {}х{}'.format(
+            *self.MIN_RESOLUTION
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        # print(img.width, img.height, img)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального')
+        return image
+
+
 
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
